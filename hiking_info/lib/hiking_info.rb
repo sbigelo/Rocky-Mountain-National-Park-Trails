6a72f26b112c12a34b297eae74dee95ec1fb2de3
Sean Bigelow <sean.bigelow1@gmail.com>
2020-12-11 22:47:20 +0000

    added gems

--- hiking_info/lib/hiking_info.rb
+++ hiking_info/lib/hiking_info.rb
@@ -1,5 +1,7 @@
 require_relative "./hiking_info/version"
 require_relative "./hiking_info/cli"
+require 'pry'
+
 module HikingInfo
   class Error < StandardError; end
   # Your code goes here...
--- hiking_info/lib/hiking_info/cli.rb
+++ hiking_info/lib/hiking_info/cli.rb
@@ -1,3 +1,4 @@
+
 class HikingInfo::CLI
   
   def call
