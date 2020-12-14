c06afb45fe00ead9bf0361ac233b77de0839f62c
Sean Bigelow <sean.bigelow1@gmail.com>
2020-12-14 19:37:57 +0000

    added @trails

--- hiking_info/lib/hiking_info/cli.rb
+++ hiking_info/lib/hiking_info/cli.rb
@@ -3,15 +3,36 @@ class HikingInfo::CLI
   
   def call
     puts "Welcome to the RMNP Trails Guide!"
-    
+    list_trails
+    trails_info
   end
   
   def list_trails
-    
+    puts "Here is the list of hiking trails to choose from:"
+    puts <<-DOC.gsub /^\s*/, ''
+      1. text 
+      2. text
+      DOC
+      @trails = HikingTrails::Trails.all
   end
   
+  
   def trails_info
-    
+    input = nil
+    while input != "exit"
+    puts "Please enter a number that is associated with the trail that you would like to know more about."
+    input = gets.strip.downcase
+    case input 
+    when "1"
+      puts "more on 1"
+    when "2"
+      puts "more on 2"
+    when "list"
+      list_trails
+    else 
+      puts "Invalid input. Type 'list' or 'exit'."
+      end
+    end
   end
   
   
