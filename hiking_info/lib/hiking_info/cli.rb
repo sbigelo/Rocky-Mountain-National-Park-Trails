556d4aea7e3bbe130aedc30d4a46a2a9fa761092
Sean Bigelow <sean.bigelow1@gmail.com>
2020-12-29 19:20:28 +0000

    changed around syntax of ordering

--- hiking_info/lib/hiking_info/cli.rb
+++ hiking_info/lib/hiking_info/cli.rb
@@ -1,13 +1,13 @@
 
 class HikingInfo::CLI
- 
+
   def call
     HikingInfo::Scraper.scrape
     welcome
     list_trails
     trails_info
   end
-  
+
   def welcome
     puts ""
     puts "   //                                              \\\\"
@@ -18,7 +18,7 @@ class HikingInfo::CLI
     puts "To see the list of hiking trails, type list."
     puts "To leave, type exit."
   end
-  
+
   def list_trails
     input = gets.strip.downcase
     if input == "list"
@@ -29,27 +29,30 @@ class HikingInfo::CLI
       trails.slice!(73)
       trails.each.with_index(1) {|trail, i| puts "#{i}. #{trail.name}"}
     elsif input == "exit"
-      system "cls"
       exit
-    else 
+    else
       puts "Incorrect input. To see the list of Hiking Trails type 'list' or to head out, type 'exit'."
       list_trails
     end
   end
-  
-  
+
+  def exit
+    # system "cls"
+  end
+
   def trails_info
-    
+    input = nil
+    while input != "exit"
     puts "Please enter a number that is associated with the trail that you would like to know more about. If you would like to exit, type 'exit'"
     input = gets.strip.downcase
-    
+
     if input.to_i > 0 && input.to_i < 81
       trail =  HikingInfo::Trails.find_by_index(input.to_i - 1)
       puts "________________________________________________________________________________"
       puts "Trail: #{trail.name}."
-      puts "Location: #{trail.location}." 
-      puts "Features: #{trail.features}." 
-      puts "Miles: #{trail.miles}." 
+      puts "Location: #{trail.location}."
+      puts "Features: #{trail.features}."
+      puts "Miles: #{trail.miles}."
       puts "Elevation Gain: #{trail.elevation_gain}."
       puts "Difficulty: #{trail.difficulty}."
       puts "Link: #{trail.link}"
@@ -61,14 +64,12 @@ class HikingInfo::CLI
       puts ""
       puts "Goodbye and thanks for visiting!"
       puts ""
-      system "cls"
+      exit
     else
-      puts "Invalid input. Type 'list' or 'exit'."
+      puts "Invalid input. Remeber to type 'list' or 'exit'."
       puts ""
-      trails_info
+      list_trails
       end
     end
+  end
 end
-  
-
-  
